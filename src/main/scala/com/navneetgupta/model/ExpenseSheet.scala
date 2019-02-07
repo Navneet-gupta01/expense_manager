7c6bef85be1d2fbad62234a029470effe8886292
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-02-07 08:58:20 +0400

    changes

--- src/main/scala/com/navneetgupta/model/ExpenseSheet.scala
+++ src/main/scala/com/navneetgupta/model/ExpenseSheet.scala
@@ -2,7 +2,6 @@ package com.navneetgupta.model
 
 import java.util.UUID
 
-import cats.data.Validated
 import cats.implicits._
 import com.navneetgupta.errors.ErrorManagement
 
@@ -24,23 +23,25 @@ final case class ClaimedExpenseSheet(id: ExpenseSheetId,
                                      expenses: List[Expense]) extends ExpenseSheet
 
 object ExpenseSheet {
+
+  import implicits._
+
   private val validateId = ErrorManagement.notNull[ExpenseSheetId]("id is null")(_)
   private val validateEmployee = ErrorManagement.notNull[Employee]("employee is null")(_)
 
-  def createOpen(id: ExpenseSheetId, employee: Employee, expenses:List[Expense]) : Validated[OpenExpenseSheet] =
-    (validateId(id), validateEmployee(employee))
-      .mapN(OpenExpenseSheet(_, _, expenses))
+  def createOpen(id: ExpenseSheetId, employee: Employee, expenses:List[Expense]) : ErrorManagement.Validated[OpenExpenseSheet] =
+    (validateId(id), validateEmployee(employee)).mapN(OpenExpenseSheet(_, _, expenses))
 
-  def createOpen(employee: Employee, expenses:List[Expense]) : Validated[OpenExpenseSheet] =
+  def createOpen(employee: Employee, expenses:List[Expense]) : ErrorManagement.Validated[OpenExpenseSheet] =
     createOpen(UUID.randomUUID(), employee, expenses)
 
   private val validateExpenses = ErrorManagement.nonEmptyList[Expense]("expenses is empty")(_)
 
-  def createClaimed(id: ExpenseSheetId, employee: Employee, expenses:List[Expense]) : Validated[ClaimedExpenseSheet] =
+  def createClaimed(id: ExpenseSheetId, employee: Employee, expenses:List[Expense]) : ErrorManagement.Validated[ClaimedExpenseSheet] =
     (validateId(id), validateEmployee(employee), validateExpenses(expenses))
       .mapN(ClaimedExpenseSheet)
 
-  def createClaimed(employee: Employee, expenses:List[Expense]) : Validated[ClaimedExpenseSheet] =
+  def createClaimed(employee: Employee, expenses:List[Expense]) : ErrorManagement.Validated[ClaimedExpenseSheet] =
     createClaimed(UUID.randomUUID(), employee, expenses)
 
   object implicits {
