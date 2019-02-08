6348d0450ecf9706d7eec4c50d1fc2d25109680c
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-02-08 21:33:18 +0400

    changes

--- src/main/scala/com/navneetgupta/services/handlers/ServicesHandler.scala
+++ src/main/scala/com/navneetgupta/services/handlers/ServicesHandler.scala
@@ -1,8 +1,12 @@
 package com.navneetgupta.services.handlers
 
 import cats.Monad
+import cats.data.NonEmptyList
+import com.navneetgupta.model.{EmployeeId, Expense, ExpenseSheet}
 import com.navneetgupta.services.Services
 
 class ServicesHandler[F[_]: Monad] extends Services[F] {
+  override def openExpense(employeeid: EmployeeId, expenses: NonEmptyList[Expense]): F[ExpenseSheet] = ???
 
+  override def claimExpense(employeeid: EmployeeId, expenses: NonEmptyList[Expense]): F[ExpenseSheet] = ???
 }
