ceb32451dc7210d661c97f581752c1d55d899857
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-02-12 08:35:28 +0400

    changes

--- src/main/scala/com/navneetgupta/services/Services.scala
+++ src/main/scala/com/navneetgupta/services/Services.scala
@@ -2,8 +2,10 @@ package com.navneetgupta.services
 
 import cats.data.NonEmptyList
 import com.navneetgupta.model.{EmployeeId, Expense, ExpenseSheet}
+import freestyle.tagless.tagless
 
+@tagless(true)
 trait Services[F[_]] {
-  def openExpense(employeeid: EmployeeId, expenses: NonEmptyList[Expense]): F[ExpenseSheet]
-  def claimExpense(employeeid: EmployeeId, expenses: NonEmptyList[Expense]): F[ExpenseSheet]
+  def openExpense(employeeid: EmployeeId, expenses: NonEmptyList[Expense]): F[Option[ExpenseSheet]]
+  def claimExpense(employeeid: EmployeeId, expenses: NonEmptyList[Expense]): F[Option[ExpenseSheet]]
 }
--- src/main/scala/com/navneetgupta/services/handlers/ServicesHandler.scala
+++ src/main/scala/com/navneetgupta/services/handlers/ServicesHandler.scala
@@ -1,12 +1,30 @@
 package com.navneetgupta.services.handlers
 
-import cats.Monad
+import cats.{Monad}
 import cats.data.NonEmptyList
-import com.navneetgupta.model.{EmployeeId, Expense, ExpenseSheet}
-import com.navneetgupta.services.Services
+import com.navneetgupta.model.{Employee, EmployeeId, Expense, ExpenseSheet}
+import com.navneetgupta.persistence.{EmployeeRepository, ExpenseSheetRepository}
+import com.navneetgupta.errors.ErrorManagement.implicits._
+import freestyle.tagless.effects.error.ErrorM
+import freestyle.tagless.logging.LoggingM
 
-class ServicesHandler[F[_]: Monad] extends Services[F] {
-  override def openExpense(employeeid: EmployeeId, expenses: NonEmptyList[Expense]): F[ExpenseSheet] = ???
+trait ServicesHandler[F[_]] {
 
-  override def claimExpense(employeeid: EmployeeId, expenses: NonEmptyList[Expense]): F[ExpenseSheet] = ???
+  implicit val M: Monad[F]
+  implicit val L: LoggingM[F]
+  val error: ErrorM[F]
+  val employeeRepo: EmployeeRepository[F]
+  val sheetRepo: ExpenseSheetRepository[F]
+
+  def openExpense(employeeid: EmployeeId, expenses: NonEmptyList[Expense]): F[Option[ExpenseSheet]] =
+    for {
+      employee <- employeeRepo.get(employeeid)
+      _ <- error.either[Employee](employee)
+      openexpenseSheet <- ExpenseSheet.createOpen(employee, expenses.toList).orRaiseError
+      created <- sheetRepo.save(openexpenseSheet)
+    } yield created
+    //ExpenseSheet.createOpen(employee, List[Expense]()).
+
+  def claimExpense(employeeid: EmployeeId, expenses: NonEmptyList[Expense]): F[Option[ExpenseSheet]] = ???
 }
+//u <- error.either[AccountEntity](account.toRight(new NoSuchElementException("Invalid User Email")))
